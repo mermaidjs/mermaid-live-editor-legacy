91df55a5acf378d72a80266c21129c1fb8d22586
Tyler Long <tyler4long@gmail.com>
2017-08-09 22:52:51 +0800

    Initial UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Row, Col, Input } from 'antd'
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        Hello mermaid live editor!
-      </div>
+      <Row gutter={16}>
+        <Col span={6}>
+          <Input.TextArea rows={6} value={'graph LR\nA-->B'} />
+        </Col>
+        <Col span={18}>
+          <h1>Hello mermaid live editor!</h1>
+        </Col>
+      </Row>
     )
   }
 }
