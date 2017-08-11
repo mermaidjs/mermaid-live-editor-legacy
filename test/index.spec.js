776c5d9a4114fc866e8033bc75af76df8af193bd
Tyler Long <tyler4long@gmail.com>
2017-08-11 14:27:08 +0800

    Test loadState

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,22 +1,24 @@
 /* eslint-env jest */
 import { Input } from 'antd'
-import axios from 'axios'
-import httpAdapter from 'axios/lib/adapters/http'
-import nock from 'nock'
 
 import App from '../src/App'
-import { getWrapper } from './shared'
+import { getWrapper, defaultValue } from './shared'
 import state from '../state.json'
 import store from './store'
-
-const host = 'http://localhost'
-axios.defaults.host = host
-axios.defaults.adapter = httpAdapter
-nock(host).get('/state.json').reply(200, { value: 'graph LR\nA-->B' })
 
 let wrapper = null
 beforeEach(() => {
   wrapper = getWrapper(App, state)
+})
+
+test('load state', () => {
+  return store.whenComplete(() => { // because it is async
+    expect(store.actions).toEqual([
+      { type: 'LOAD_STATE' },
+      { type: 'SET_STATE', state: defaultValue }
+    ])
+    expect(store.getState()).toEqual(defaultValue)
+  })
 })
 
 test('edit value', () => {
--- test/shared.js
+++ test/shared.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
+import nock from 'nock'
+import axios from 'axios'
+import httpAdapter from 'axios/lib/adapters/http'
 
 import store from './store'
 import { setState } from '../src/actions'
 
+const host = 'http://localhost'
+axios.defaults.host = host
+axios.defaults.adapter = httpAdapter
+
+export const defaultValue = { value: 'graph LR\nA-->B' }
+
 export const getWrapper = (Component, state) => {
+  nock(host).get('/state.json').reply(200, defaultValue)
   store.dispatch(setState(state))
   store.resetActions()
   return mount(
