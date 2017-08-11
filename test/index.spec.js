5a4c92055088011071fc7f3a2960b37bba409d0d
Tyler Long <tyler4long@gmail.com>
2017-08-11 11:00:02 +0800

    Mock http using nock

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,18 @@
 /* eslint-env jest */
 import { Input } from 'antd'
+import axios from 'axios'
+import httpAdapter from 'axios/lib/adapters/http'
+import nock from 'nock'
 
 import App from '../src/App'
 import { getWrapper } from './shared'
 import state from '../state.json'
 import store from './store'
+
+const host = 'http://localhost'
+axios.defaults.host = host
+axios.defaults.adapter = httpAdapter
+nock(host).get('/state.json').reply(200, { value: 'graph LR\nA-->B' })
 
 let wrapper = null
 beforeEach(() => {
